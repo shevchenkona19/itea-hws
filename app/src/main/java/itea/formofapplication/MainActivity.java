5c06bb36703de87670c8b00909f5ad70f9a8bbcf
Nikita Shevchenko <shevchenkona19@gmail.com>
2016-11-27 20:23:20 +0200

    Done all the programming

--- app/src/main/java/itea/formofapplication/MainActivity.java
+++ app/src/main/java/itea/formofapplication/MainActivity.java
@@ -2,12 +2,49 @@ package itea.formofapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import java.util.jar.Attributes;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnSend;
+    private RadioGroup rgWorkSchedule;
+    private EditText etName, etLastName, etEmail;
+    private MyTextWatcher textWatcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSend = (Button) findViewById(R.id.btnSend);
+        rgWorkSchedule = (RadioGroup) findViewById(R.id.rgWorkSchedule);
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etLastName = (EditText) findViewById(R.id.etLastName);
+        etName = (EditText) findViewById(R.id.etName);
+
+        textWatcher = new MyTextWatcher(new EditText[] {etName, etLastName, etEmail});
+
+        etName.addTextChangedListener(textWatcher);
+        etEmail.addTextChangedListener(textWatcher);
+        etLastName.addTextChangedListener(textWatcher);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(rgWorkSchedule.getCheckedRadioButtonId() == -1 ){
+                    Toast.makeText(MainActivity.this, "You haven't choose a work schedule", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Toast.makeText(MainActivity.this, textWatcher.getName() + " " + textWatcher.getLastName() + " " + textWatcher.getEmail(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
